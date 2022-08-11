bf16bde0e870ca095b1c92a3bd8ecbfe2e9a46fd
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-11 13:43:01 +0600

    runner added

---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,10 @@
+package testRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
